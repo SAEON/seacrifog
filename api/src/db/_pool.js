4baa7f2fa3abf30910cbddcf8b983fddfc185381
Zach Smith <zachsmith.sa@gmail.com>
2022-04-01 14:21:35 +0200

    Updated to use external postgres

--- api/src/db/_pool.js
+++ api/src/db/_pool.js
@@ -2,11 +2,11 @@ import { config } from 'dotenv'
 import { Pool } from 'pg'
 config()
 
-const DB = process.env.POSTGRES_DATABASE || 'seacrifog'
 const POSTGRES_HOST = process.env.POSTGRES_HOST || 'localhost'
+const POSTGRES_PORT = parseInt(process.env.POSTGRES_PORT, 10) || 5432
+const DB = process.env.POSTGRES_DATABASE || 'seacrifog'
 const POSTGRES_USER = process.env.POSTGRES_USER || 'postgres'
 const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || 'password'
-const POSTGRES_PORT = parseInt(process.env.POSTGRES_PORT, 10) || 5432
 
 export default () =>
   new Pool({
